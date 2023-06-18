70ede8d555d2a9bcbc550757ea02a3ccda58cbd7
kshitijdalvi4 <111135860+kshitijdalvi4@users.noreply.github.com>
2023-06-18 17:59:48 +0530

    Basic Functionality v1.0

--- JS/site.js
+++ JS/site.js
@@ -1,6 +1,65 @@
 
-//Function Call -> helloWorld
-function helloWorld() {
+//Get the Values from inputs
+//controll
+function getVal() {
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    
+   //str parsed to int
+    startValue=parseInt(startValue);
+    endValue=parseInt(endValue);
 
-    alert("Hello World");
+   
+
+    if(Number.isInteger(startValue) && Number.isInteger(endValue))
+    {
+         let num=generateNum(startValue,endValue);
+             //Display Func
+                 displayNum(num);
+
+    }
+
+    else{
+        alert("Error: Enter Integers");
+    }
+
+  
 }
+
+//logic
+function generateNum(start,end)
+{
+  let num=[];
+
+for(let index=start; index<=end; index++){
+    //index is not a keywiord
+     num.push(index);
+ }
+  return num;
+}
+
+
+//display nums
+function displayNum(num){
+    
+ let templateRows=[];
+    for(let i=0; i<num.length; i++){
+        
+        let numbers = num[i];
+        let className ="even";
+
+        if (numbers %2 ==0)
+        {
+            className="even";
+        }
+
+        else{
+            className="odd";
+        }
+        templateRows += `<tr><td class="${className}"> ${numbers} </td></tr>`;
+      
+    }
+    
+    document.getElementById("results").innerHTML=templateRows;
+}
+
